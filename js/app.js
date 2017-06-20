872cea4bd622eccbe51f6814fc1900c47927a876
Rami Bououni <rami.bououni@gmail.com>
2017-06-19 19:37:43 -0700

    created random img name and render image functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
+'use strict';
 
+var imageName1 = '';
+var imageName2 = '';
+var imageName3 = '';
+var imageParent;
+var duplicate = [];
+
+var imageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+//test generateRandomImageName
+console.log(generateRandomImageName());
+//test random number
+console.log(Math.floor(Math.random() * imageNames.length));
+
+setup();
+
+//generate random image name
+function generateRandomImageName() {
+  var index = Math.floor(Math.random() * imageNames.length);
+  return imageNames[index];
+}
+
+// render images
+function renderImage(imgName) {
+  imageParent = document.getElementById('images');
+  var img = document.createElement('img');
+  //set extensions for the images
+  if (imgName === 'sweep') {
+    img.setAttribute('src', 'img/' + imgName + '.png');
+    imageParent.appendChild(img);
+  } else if (imgName === 'usb') {
+    img.setAttribute('src', 'img/' + imgName + '.gif');
+    imageParent.appendChild(img);
+  } else {
+    img.setAttribute('src', 'img/' + imgName + '.jpg');
+    imageParent.appendChild(img);
+  }
+}
